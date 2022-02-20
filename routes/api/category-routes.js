24d45fac1ca229c49360165dd8a9b1c8e0924d65
Jorge A Salinas Jr <jsalinas212@outlook.com>
2022-02-20 17:50:56 -0600

    working on api routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,24 +5,59 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
+  try {
+    const cat = await Category.findAll({
+      include: [Product],
+    });
+
+    res.json(cat);
+
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
+  try {
+    const cat = await Category.findOne({
+      where: {
+        id: req.paarams.id, // ID from request
+      },
+      include: [Product],
+    });
+
+    res.json(cat);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+
+  } catch (err) {
+
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+
+  } catch (err) {
+
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+
+  } catch (err) {
+
+  }
 });
 
 module.exports = router;
